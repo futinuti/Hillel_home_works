b7b1bf7a3c52f1def2d59d93fa5722cbae3cf1ac
futinuti <amihailovp@gmail.com>
2022-10-26 17:42:54 +0300

    исправил, диапазон и проверку по делению на 100 и 400

--- domashka_4/leap_year_4.py
+++ domashka_4/leap_year_4.py
@@ -1,13 +1,16 @@
-god = input("Enter a year to check 1900 -- 1 000 000  :")
+god = input("Enter a year to check 1900 -- 1000000  :")
 
 if not god.isdigit():  # на случай, если ввели буквы
     print("The year does not meet the criteria, please enter an integer")
 else:
     god = int(god)
-    if god < 1900 or god > 1000000:
-        print("The range does not meet the conditions ")
-    else:
-        if god % 4 == 0 or god % 400 == 0 or god % 100 == 0:
-            print(god, "is leap year ")
+    if 1900 < god < 1000000:
+        if god % 4 == 0:
+            if god % 400 != 0 and god % 100 == 0:
+                print(god, "is not leep year ")
+            else:
+                print(god, "is leep year")
         else:
             print(god, "is not leap year")
+    else:
+        print("The range does not meet the conditions ")
