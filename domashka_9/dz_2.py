40a1bf5d2d7e339fca68114d2cd7e7257b0b9190
futinuti <amihailivp@gmail.com>
2022-11-13 16:07:23 +0200

    добавил 2 задание к ДЗ 9

---
+++ domashka_9/dz_2.py
@@ -0,0 +1,21 @@
+import random
+
+n = int(input("Input N :"))
+
+random_matrix = [[random.randint(0, 10)
+                 for row in range(n)]
+                 for column in range(n)]
+
+sum_diagonals = sum([random_matrix[i][i] for i in range(n)])
+# второстепенная диагональ
+# sum_diagonals_2 = sum([random_matrix[i][n-1-i] for i in range(n)])
+sum_last_column = sum([random_matrix[i][n - 1] for i in range(n)])
+
+for row in random_matrix:
+    print("[", end=" ")
+    for elem in row:
+        print(f'{elem:^4}', end=" ")
+    print("]")
+print(f'Sum of diagonals \\ : {sum_diagonals:.3f}')
+# print(f'Sum of diagonals / : {sum_diagonals_2:.3f}')
+print(f'Sum of last columns: {sum_last_column:.3f}')
