39ca2ffb05959ae128a2e3020a33c6f10786f3b1
futinuti <amihailivp@gmail.com>
2022-11-14 18:41:54 +0200

    Исправил РЕР-8

--- domashka_9/dz_1.py
+++ domashka_9/dz_1.py
@@ -1,5 +1,6 @@
 n = int(input("Input N "))
-matrix_1 = [[row if column % 2 != 0 else column for row in range(-n, 0)] for column in range(1, n + 1)]
+matrix_1 = [[row if column % 2 != 0 else column for row in range(-n, 0)]
+            for column in range(1, n + 1)]
 
 for row in matrix_1:
     print("[", end=" ")
