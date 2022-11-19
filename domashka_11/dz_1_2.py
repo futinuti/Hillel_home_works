404d71c3ab579503d689f38a7f51583b8ddde6f8
futinuti <amihailivp@gmail.com>
2022-11-19 11:17:20 +0200

    N11(dz_1_2)

---
+++ domashka_11/dz_1_2.py
@@ -0,0 +1,10 @@
+import random as rd
+
+# первое (берем длину множества сгенерированого списка)
+print(len(set(rd.randint(1, 100) for i in range(0, 20))))
+
+# второе (тоже самое только от обьединенных множеств)
+print(len(
+    set(rd.randint(1, 10) for j in range(0, 10)) |
+    set(rd.randint(5, 15) for k in range(0, 10)))
+)
