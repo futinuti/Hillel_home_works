ed9b7e7f119e54c2bc8e53a95dcbdf128ec21c41
futinuti <amihailivp@gmail.com>
2022-11-20 19:22:23 +0200

    Revert "N12dz_2"
    This reverts commit f83f7e0568253b4d6e9e7a366b49062a43e084d6.

--- domashka_12/dz_2.py
+++
@@ -1,5 +0,0 @@
-kvadrat = map(lambda x, y=2: x ** y, range(1, 8))
-print(list(kvadrat))  # берет только 'х'
-
-kvadrat = map(lambda x, y=2: x ** y, range(11, 18), range(1, 8))
-print(list(kvadrat))  # берет и 'x', и 'y'
