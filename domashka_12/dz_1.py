99e37beff0bb162d6b480e25b3ebfda53b2caf29
futinuti <amihailivp@gmail.com>
2022-11-20 14:53:15 +0200

    N12dz_1

---
+++ domashka_12/dz_1.py
@@ -0,0 +1,27 @@
+import random as rd
+
+
+def test_sum_1(list_1, var_1):
+    """
+    :param list_1: list or generator
+    :param var_1: var for if
+    :return: Bool
+    """
+    # list_2 = list(list_1)
+    list_2 = list_1
+    for i in range(len(list_2)):
+        for j in range(len(list_2)):
+            if i == j:  # Исключает сложение самого с собой
+                break
+            if list_1[i] + list_1[j] == var_1:
+                return True
+    return False
+
+
+print(test_sum_1(range(1, 11000, 100), 502))
+print(test_sum_1(range(1, 11), 2))
+print(test_sum_1((1, 2, 3, 4, 5, 6, 7, 8, 9, 10), 19))
+listik = []
+for k in range(1, 10):
+    listik.append(rd.randint(1, 1 * 10 ** 12))
+print(test_sum_1(listik, 19))
