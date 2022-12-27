4f601b89618f28e0ad92081d7f6469c63ef39d77
Futinuti <amihailovp@gmail.com>
2022-12-27 09:45:43 +0200

    МБ создаются корабли

--- mb_2.py
+++ mb_2.py
@@ -1,42 +1,70 @@
-from my_module import visioner_list
-from random import randint
-class Ship():
+# from my_module import visioner_list
+from random import randrange
+
+
+class Ship:
     palubi = 1
     orientation = 1
     point_ship = []
     point_nearby = []
     ship_ok = 1
+    start_coord = ''
 
     def __init__(self, palubi, orientation, start_coord):
         self.palubi = palubi
         self.orientation = orientation
         self.point_ship = []
         self.point_nearby = []
-        self.ship_ok = 1
+        self.ship_ok = 0
+        self.start_coord = start_coord
 
-    pass
+        # проверка на то что корабль влазит в игровое поле
+
+        while self.ship_ok == 0:
+            if orientation == 0 and int(start_coord[0]) + palubi <= 9:
+                self.ship_ok = 1
+                for i in range(1, palubi+1):
+                    gorizont = i + int(start_coord[0])
+                    vertical = int(start_coord[2])
+                    self.point_ship.append(str(gorizont) + ';' + str(vertical))
+            if orientation == 1 and int(start_coord[2]) + palubi <= 9:
+                self.ship_ok = 1
+                for i in range(1, palubi+1):
+                    gorizont = int(start_coord[0])
+                    vertical = i + int(start_coord[2])
+                    self.point_ship.append(str(gorizont) + ';' + str(vertical))
+            else:
+                self.ship_ok = 0
+                break
+
+            print(self.ship_ok, orientation)
+            print(self.point_ship)
 
 class Board:
     letters = ['ABCDEFJKLM']
     coord_field = []
+    orient = 1
     #  draw empty
+
     def draw_coord_empty_field(self):
-        for i in range(1, 11): # x-stroka
-            for j in range(1, 11): # y-stolbec
+        for i in range(10):  # x-stroka
+            for j in range(10):  # y-stolbec
                 x_y = str(i) + ';' + str(j)
                 self.coord_field.append(x_y)
         return self.coord_field
 
-    # создаю корабль с произвольными начальными координатами
-    rand_start_coord = str(randint(1, 11)) + ';' + str(randint(1, 11))
-    print(rand_start_coord)
-    for i in range(4,0,-1):  # палубность 4-1
-        for j in range(1,5): # количество кораблей
 
-            
+    # print(rand_start_coord)
+    for i in range(5, 0, -1):  # палубность 4-1
+        for j in range(5 - i):  # количество кораблей
+            while True:
+                orient = randrange(2)  # произвольная ориентация
+                # создаю корабль с произвольными начальными координатами
+                rand_start_coord = str(randrange(10)) + ';' + str(randrange(10))
+                ship = Ship(i, orient, rand_start_coord)
+                if ship.ship_ok == 1:
+                    break
 
 
 bd = Board()
-visioner_list(bd.draw_coord_empty_field())
-
-
+# visioner_list(bd.draw_coord_empty_field())
