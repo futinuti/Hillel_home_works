26de1d92e5d95689fd1033a635a1d1d6301361af
Futinuti <amihailovp@gmail.com>
2022-12-27 10:47:34 +0200

    МБ создал точки вокруг кораблей

--- mb_2.py
+++ mb_2.py
@@ -21,24 +21,34 @@ class Ship:
         # проверка на то что корабль влазит в игровое поле
 
         while self.ship_ok == 0:
-            if orientation == 0 and int(start_coord[0]) + palubi <= 9:
+
+            if self.orientation == 0 and int(start_coord[0]) + palubi <= 9:
                 self.ship_ok = 1
                 for i in range(1, palubi+1):
                     gorizont = i + int(start_coord[0])
                     vertical = int(start_coord[2])
                     self.point_ship.append(str(gorizont) + ';' + str(vertical))
-            if orientation == 1 and int(start_coord[2]) + palubi <= 9:
+                    for k in range(3):
+                        for j in range(palubi + 2):
+                            self.point_nearby.append(str(gorizont - 1 + j) +
+                                                ';' + str(vertical - 1 + k))
+            elif self.orientation == 1 and int(start_coord[2]) + palubi <= 9:
                 self.ship_ok = 1
                 for i in range(1, palubi+1):
                     gorizont = int(start_coord[0])
                     vertical = i + int(start_coord[2])
                     self.point_ship.append(str(gorizont) + ';' + str(vertical))
+                    for k in range(3):
+                        for j in range(palubi + 2):
+                            self.point_nearby.append(str(gorizont - 1 + j) + ';' +
+                                                     str(vertical - 1 + k))
             else:
                 self.ship_ok = 0
                 break
 
             print(self.ship_ok, orientation)
             print(self.point_ship)
+            print(self.point_nearby)
 
 class Board:
     letters = ['ABCDEFJKLM']
@@ -54,15 +64,17 @@ class Board:
         return self.coord_field
 
 
-    # print(rand_start_coord)
+    # Создаём корабли
     for i in range(5, 0, -1):  # палубность 4-1
         for j in range(5 - i):  # количество кораблей
             while True:
                 orient = randrange(2)  # произвольная ориентация
+
                 # создаю корабль с произвольными начальными координатами
                 rand_start_coord = str(randrange(10)) + ';' + str(randrange(10))
                 ship = Ship(i, orient, rand_start_coord)
                 if ship.ship_ok == 1:
+
                     break
 
 
